fb2a4cdfe1a8b9045078aad366ca55369e743e74
Damilola Kolawole <61382051+Baouldguyd@users.noreply.github.com>
2023-06-07 13:52:35 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+// import userinput and useroutput components to be used in our app
 import UserInput from "./component/userInput"
 import UserOutput from "./component/userOutput"
 
